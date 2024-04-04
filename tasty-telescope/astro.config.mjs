803c1dea5e9363d56bdb2d9249adb8a4c9076a5d
Jhonatec <jhonsreis@gmail.com>
2024-04-04 15:31:28 -0300

    feat: add preact island

--- tasty-telescope/astro.config.mjs
+++ tasty-telescope/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro.jhonatec.dev"
+  site: "https://astro.jhonatec.dev",
+  integrations: [preact()]
 });
